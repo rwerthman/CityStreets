d1353425fc2f0750e9d3c725cbf86543aa16a6bd
Bobby <bobby@Bobbys-MacBook-Pro.local>
2022-04-16 17:30:21 -0400

    Roads have a popup now and change color when clicked.

--- scripts/main.js
+++ scripts/main.js
@@ -5,20 +5,51 @@ var map = new mapboxgl.Map({
     center: [-83.4702, 42.3714],
     zoom: 14 // starting zoom
 });
+var clickedStateId = null;
 map.on('load', function () {
     map.addSource('plymouth_roads', {
         type: 'geojson',
-        data: '../geojson/plymouth_roads.geojson'
+        data: '../geojson/plymouth_roads.geojson',
+        generateId: true
     });
     map.addLayer({
         'id': 'plymouth_roads',
         'type': 'line',
         'source': 'plymouth_roads',
         'paint': {
-            'line-color': '#03f',
             'line-width': 5,
-            'line-opacity': 0.6
+            'line-opacity': 0.6,
+            'line-color': [
+                'case',
+                ['boolean', ['feature-state', 'clicked'], false],
+                '#f50',
+                '#03f' // blue
+            ]
         },
         'filter': ['==', '$type', 'LineString']
+    });
+    map.on('click', 'plymouth_roads', function (e) {
+        // Change the clicked road back to its unclicked color
+        if (clickedStateId !== null) {
+            map.setFeatureState({ source: 'plymouth_roads', id: clickedStateId }, { clicked: false });
+        }
+        // Save the id of the currently clicked road
+        clickedStateId = e.features[0].id;
+        // Change the color of a clicked road so it stands out        
+        map.setFeatureState({ source: 'plymouth_roads', id: clickedStateId }, { clicked: true });
+        // Add a popup to the road when it is clicked
+        var name = e.features[0].properties.name;
+        var popup = new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(name)
+            .addTo(map);
+    });
+    // Change the cursor to a pointer when the it enters a feature in the 'plymouth_roads' layer.
+    map.on('mouseenter', 'plymouth_roads', function () {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'plymouth_roads', function () {
+        map.getCanvas().style.cursor = '';
     });
 });
--- scripts/main.ts
+++ scripts/main.ts
@@ -6,11 +6,14 @@ const map = new mapboxgl.Map({
     zoom: 14 // starting zoom
 });
 
+let clickedStateId = null;
+
 map.on('load', () => {
     
     map.addSource('plymouth_roads', {
         type: 'geojson',
-        data: '../geojson/plymouth_roads.geojson'
+        data: '../geojson/plymouth_roads.geojson',
+        generateId: true
     });
 
     map.addLayer({
@@ -18,11 +21,53 @@ map.on('load', () => {
         'type': 'line',
         'source': 'plymouth_roads',
         'paint': {
-            'line-color': '#03f', // @todo: use #f50 for orange color for on clicked like overpass-turbo.eu
             'line-width': 5,
-            'line-opacity': 0.6
+            'line-opacity': 0.6,
+            'line-color': [
+                'case',
+                ['boolean', ['feature-state', 'clicked'], false],
+                '#f50', // orange
+                '#03f' // blue
+            ]            
         },
         'filter': ['==', '$type', 'LineString']
+    });
+
+    map.on('click', 'plymouth_roads', (e) => {
+
+        // Change the clicked road back to its unclicked color
+        if (clickedStateId !== null) {
+            map.setFeatureState(
+                { source: 'plymouth_roads', id: clickedStateId },
+                { clicked: false }
+            );
+        }
+
+        // Save the id of the currently clicked road
+        clickedStateId = e.features[0].id;
+
+        // Change the color of a clicked road so it stands out        
+        map.setFeatureState(
+            { source: 'plymouth_roads', id: clickedStateId },
+            { clicked: true }
+        );
+
+        // Add a popup to the road when it is clicked
+        const name = e.features[0].properties.name;
+        const popup = new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(name)
+            .addTo(map);        
+    });
+
+    // Change the cursor to a pointer when the it enters a feature in the 'plymouth_roads' layer.
+    map.on('mouseenter', 'plymouth_roads', () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+     
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'plymouth_roads', () => {
+        map.getCanvas().style.cursor = '';
     });
 
 });
