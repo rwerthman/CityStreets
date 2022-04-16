176a29e7f2b0848120e17699c426932bb3bafb0f
Bobby <bobby@Bobbys-MacBook-Pro.local>
2022-04-16 11:33:26 -0400

    Added some geojson for plymouth roads. Still need to be able to click those roads and test the person on them.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,24 @@
-mapboxgl.accessToken = '<token>';
+mapboxgl.accessToken = 'pk.eyJ1IjoibGF0aXR1ZGU5OCIsImEiOiJjbDF6N3NrdDgwMW5uM2VwNnB3MjJoZGJhIn0.LaHND5ulTYyHOEblUg9MwA';
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-74.5, 40],
-    zoom: 9 // starting zoom
+    center: [-83.4702, 42.3714],
+    zoom: 14 // starting zoom
+});
+map.on('load', function () {
+    map.addSource('plymouth_roads', {
+        type: 'geojson',
+        data: '../geojson/plymouth_roads.geojson'
+    });
+    map.addLayer({
+        'id': 'plymouth_roads',
+        'type': 'line',
+        'source': 'plymouth_roads',
+        'paint': {
+            'line-color': '#03f',
+            'line-width': 5,
+            'line-opacity': 0.6
+        },
+        'filter': ['==', '$type', 'LineString']
+    });
 });
--- scripts/main.ts
+++ scripts/main.ts
@@ -1,7 +1,28 @@
-mapboxgl.accessToken = '<token>';
+mapboxgl.accessToken = 'pk.eyJ1IjoibGF0aXR1ZGU5OCIsImEiOiJjbDF6N3NrdDgwMW5uM2VwNnB3MjJoZGJhIn0.LaHND5ulTYyHOEblUg9MwA';
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    center: [-83.4702, 42.3714], // starting position [lng, lat]
+    zoom: 14 // starting zoom
+});
+
+map.on('load', () => {
+    
+    map.addSource('plymouth_roads', {
+        type: 'geojson',
+        data: '../geojson/plymouth_roads.geojson'
+    });
+
+    map.addLayer({
+        'id': 'plymouth_roads',
+        'type': 'line',
+        'source': 'plymouth_roads',
+        'paint': {
+            'line-color': '#03f', // @todo: use #f50 for orange color for on clicked like overpass-turbo.eu
+            'line-width': 5,
+            'line-opacity': 0.6
+        },
+        'filter': ['==', '$type', 'LineString']
+    });
+
 });
