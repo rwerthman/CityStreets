3bb15e56cbd6b33774d5620896844b997d5c2516
Bobby <bobby@Bobbys-MacBook-Pro.local>
2022-04-20 12:13:09 -0400

    Testing adding places to the map

--- scripts/main.js
+++ scripts/main.js
@@ -13,22 +13,61 @@ map.on('load', function () {
         data: '../geojson/plymouth_roads.geojson',
         generateId: true
     });
-    map.addLayer({
-        'id': 'plymouth_roads',
-        'type': 'line',
-        'source': 'plymouth_roads',
-        'paint': {
-            'line-width': 5,
-            'line-opacity': 0.6,
-            'line-color': [
-                'case',
-                ['boolean', ['feature-state', 'clicked'], false],
-                '#f50',
-                '#03f' // blue
-            ]
-        },
-        'filter': ['==', '$type', 'LineString']
+    map.addSource('plymouth_places', {
+        type: 'geojson',
+        data: '../geojson/plymouth_places.geojson',
+        generateId: true
     });
+    // Add a new layer to visualize the polygon.
+    // map.addLayer({
+    //     'id': 'plymouth_places',
+    //     'type': 'fill',
+    //     'source': 'plymouth_places',
+    //     'layout': {},
+    //     'paint': {
+    //         'fill-color': '#0080ff', // blue color fill
+    //         'fill-opacity': 0.5
+    //     },
+    //     'filter': ['has', 'name']
+    // });
+    // Add a black outline around the polygon.
+    map.addLayer({
+        'id': 'outline',
+        'type': 'line',
+        'source': 'plymouth_places',
+        'layout': {},
+        'paint': {
+            'line-color': '#03f',
+            'line-width': 5,
+            'line-opacity': 0.6
+        },
+        'filter': ['has', 'name']
+    });
+    // When a click event occurs on a feature in the states layer,
+    // open a popup at the location of the click, with description
+    // HTML from the click event's properties.
+    map.on('click', 'plymouth_places', function (e) {
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.name)
+            .addTo(map);
+    });
+    // map.addLayer({
+    //     'id': 'plymouth_roads',
+    //     'type': 'line',
+    //     'source': 'plymouth_roads',
+    //     'paint': {
+    //         'line-width': 5,
+    //         'line-opacity': 0.6,
+    //         'line-color': [
+    //             'case',
+    //             ['boolean', ['feature-state', 'clicked'], false],
+    //             '#f50', // orange
+    //             '#03f' // blue
+    //         ]            
+    //     },
+    //     'filter': ['==', '$type', 'LineString']
+    // });
     map.on('click', 'plymouth_roads', function (e) {
         // Change the clicked road back to its unclicked color
         if (clickedStateId !== null) {
